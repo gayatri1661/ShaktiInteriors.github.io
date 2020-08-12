47a802228a7ec1beffc65036a8c6c2b087474760
gayatri1661 <43004481+gayatri1661@users.noreply.github.com>
2020-08-12 07:01:52 +0530

    First Commit

--- src/main/java/com/rakhi/sales/Controller/Login.java
+++ src/main/java/com/rakhi/sales/Controller/Login.java
@@ -28,7 +28,7 @@ public class Login {
 	@Autowired
 	Userrepository Userepository;
 	
-	@RequestMapping(value="/shaktiinteriors1",method=RequestMethod.GET)
+	@RequestMapping(value= {"/","/shaktiinteriors"},method=RequestMethod.GET)
 	public String welcomemsg() {
 		return "welcome";
 	}
